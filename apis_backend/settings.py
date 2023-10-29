f57ba94a9362bf1d555eafb62d108a52b628839f
luiz ignacio <quineche44@gmail.com>
2023-10-28 21:33:20 -0500

    files moved

--- apis_backend/settings.py
+++ apis_backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-o2yubtys8m6(dwc-891n%wum_&=0(*p1%*kk5&c$@82*(hg+qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -118,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#################################################################
+# for deployment
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # new
+
+#################################################################
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
