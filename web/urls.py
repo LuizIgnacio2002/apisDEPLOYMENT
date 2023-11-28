d5bc63aa7f8bd539390e16fefd4601d45c97bc2f
LuisLTdev <135352503+LuisLTdev@users.noreply.github.com>
2023-11-27 23:07:56 -0500

    Update urls.py

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path('sendImg/', send_img, name="sendImg"),
+    # path("", views.index, name = "index"),
+    path('sendImg/', views.send_img, name="sendImg"),
 ]
