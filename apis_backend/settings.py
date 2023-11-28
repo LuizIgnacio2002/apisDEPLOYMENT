54b2c1737d11d7ccfc5a83210108a1a0e9004fa9
Luiz2002 <quineche44@gmail.com>
2023-11-28 14:49:01 +0000

    degub false

--- apis_backend/settings.py
+++ apis_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o2yubtys8m6(dwc-891n%wum_&=0(*p1%*kk5&c$@82*(hg+qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
